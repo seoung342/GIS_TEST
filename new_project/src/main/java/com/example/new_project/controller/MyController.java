a1729c77f019dd0a00576c1ecd7850938d9ce02c
seoung342 <seoung342@gmail.com>
2024-12-09 16:23:50 +0900

    202412091623

--- new_project/src/main/java/com/example/new_project/controller/MyController.java
+++ new_project/src/main/java/com/example/new_project/controller/MyController.java
@@ -1,16 +1,22 @@
 package com.example.new_project.controller;
 
+import com.example.new_project.entity.Frequently_Question;
+import com.example.new_project.service.FAQService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 @CrossOrigin(origins = "http://localhost:9999")
+@RequiredArgsConstructor
 public class MyController {
 
-    @Autowired
-
+    private final FAQService faqService;
 
     @GetMapping("/")
     public String index() {
@@ -23,7 +29,9 @@ public class MyController {
     }
 
     @GetMapping("/faq")
-    public String faq(){
-        return "/faq";
+    public String faq(Model model) throws Exception {
+        List<Frequently_Question> faq = faqService.selectAllFAQ();
+        model.addAttribute("faq", faq);
+        return "/faq/faqList";
     }
 }
